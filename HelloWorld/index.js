c50469fcba542c198477f2f9f9f3723609ae9d17
IlitA <imilitamar@gmail.com>
2021-02-18 17:37:03 +0100

    cleanup

--- HelloWorld/index.js
+++
@@ -1,8 +0,0 @@
-var nameInput = document.getElementById('name');
-var greet = document.getElementById('greet');
-
-nameInput.addEventListener('keyup', displayName);
-
-function displayName() {
-    greet.textContent = 'Hello ' + nameInput.value + '!';
-}
---
+++ index.js
@@ -0,0 +1,7 @@
+var buttonElement = document.getElementById('groverButton');
+
+buttonElement.addEventListener('click', displayName);
+
+function displayName() {
+    console.log('click');
+}
