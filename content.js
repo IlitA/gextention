690f52965e2beb20debab4de00d0466252c4118b
Marc Müller <marc@mueller.dev>
2021-02-18 20:21:55 +0100

    Cleaned up code and added image cases
    Changes include:
    • Cleaned up the code
    • Renamed parsedInfo to parsedData
    • Added special cases for scrapping images from Conrad and idealo
    • Updated the way that the title is parsed
    • Added the hostname to the parsedData

--- content.js
+++ content.js
@@ -1,26 +1,38 @@
-const parsedInfo = {}
-parsedInfo.url = window.location.href;
+const parsedData = {}
+parsedData.url = window.location.href;
 const {
     hostname
-} = new URL(parsedInfo.url);
+} = new URL(parsedData.url);
+parsedData.hostname = hostname;
 
 window.onload = () => {
+    // Try getting image from og:image meta tags
+    parsedData.imageUrl = document?.head?.querySelector('meta[property="og:image"]')?.content || '';
+    if (!parsedData.imageUrl) parsedData.imageUrl = document?.head?.querySelector('meta[name="og:image"]')?.content || ''; // Case: Gravis
+
+    // Special cases
     switch (hostname.split('.')[1]) {
         case 'amazon':
-            parsedInfo.productName = document?.querySelector('#productTitle')?.innerText || '';
-            parsedInfo.imageUrl = document?.querySelector('#imgTagWrapperId img')?.src || '';
+            parsedData.productName = document?.querySelector('#productTitle')?.innerText || '';
+            parsedData.imageUrl = document?.querySelector('#imgTagWrapperId img')?.src || '';
             break;
-        default:
-            parsedInfo.productName = document?.head?.querySelector('title')?.innerText || '';
-            parsedInfo.imageUrl = document?.head?.querySelector('meta[property="og:image"]')?.content || '';
+        case 'conrad':
+            parsedData.imageUrl = document?.querySelector('#productMainImage')?.src || '';
+            break;
+        case 'idealo':
+            parsedData.imageUrl = document?.querySelector('img.productReviews-image')?.src || '';
+            break;
     }
+
+    // If no special case, or product name results in '', get title from document head
+    if (!parsedData.productName) parsedData.productName = document?.head?.querySelector('title')?.innerText || '';
 }
 
 chrome.runtime.onMessage.addListener(
-    function (message, sender, sendResponse) {
+    (message, sender, sendResponse) => {
         switch (message.type) {
-            case 'getParsedInfo':
-                sendResponse(parsedInfo);
+            case 'getParsedData':
+                sendResponse(parsedData);
                 break;
             default:
                 console.error('Unrecognised message: ', message);
