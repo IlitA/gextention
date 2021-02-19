82b3d285fb55bfc4bac4365efcb67b59222a8b7c
IlitA <imilitamar@gmail.com>
2021-02-19 10:09:29 +0100

    temp

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var buttonElement = document.getElementById('groverButton');
+var newProduct = document.getElementById('product');
 
 buttonElement.addEventListener('click', displayName);
 
@@ -13,6 +14,13 @@ chrome.tabs.query({active: true, currentWindow: true}, tabs => {
             console.error('Error whilst parsing the page information');
         } else {
             console.log(parsedData);
+
+            const img = document.createElement("img");
+img.setAttribute("src", parsedData.imageUrl);
+
+            newProduct.appendChild(img);
+
+            // imageUrl
             // Prefill the form with the parsed information & have a hidden field with the url
         }
     });
